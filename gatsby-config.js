eaa08ca6bc4f9003c8960b9b63b529b8fa06c751
Sima <sima.lotfi@gmail.com>
2019-10-09 20:36:03 -0700

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    title: '0Browser Blog',
+    description: '0Browser Blog',
+    siteUrl: 'https://blog.0browser.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
-        siteUrl: 'https://gatsby-casper.netlify.com',
+        siteUrl: 'https://blog.0browser.com',
       },
     },
     'gatsby-plugin-emotion',
